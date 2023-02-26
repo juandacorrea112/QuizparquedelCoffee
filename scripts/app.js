8807350c79634faf5e4d63a4c45e76e150bb4387
juandavidad <juandavidcorreaorozco74@gmail.com>
2023-02-26 16:52:33 -0500

    comienzo del quiz parque del cafe

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+//IR AL QUIZ
+const btnstarQ = document.getElementById('ComenzarQ')
+
+btnstarQ.addEventListener('click', Goquiz)
+
+function Goquiz() {
+    window.location.assign("/pages/quiz.html")
+}
+
