88b20ba6f92f860728bc0864782cd5eb30f23474
juandavidad <juandavidcorreaorozco74@gmail.com>
2023-03-01 17:49:44 -0500

    pruebas

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,19 @@
 //         window.location.assign("/pages/quiz.html")
 //     },3000)
 // })
+
+
 //FUNCIONALIDAD DE MOSTRAR LAS PREGUNTAS
 document.getElementById("cardBtn").addEventListener("click", () => {
     document.getElementById("container").classList.toggle("change")
+})
+
+//SELECCIONAR UNA OPCION PARA RESPONDER
+
+document.getElementById("opcionA").addEventListener('click', () => {
+    document.getElementById("opcionA").style.background = "rgb(0, 255, 0)"
+    setTimeout(() => {        
+        document.getElementById("container").classList.toggle("change")
+    }, 900);
+   
 })
