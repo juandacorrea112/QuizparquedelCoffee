b1f560245ae9f9f52b4e0cc0bfe4fc05c991ad0f
juandavidad <juandavidcorreaorozco74@gmail.com>
2023-03-01 15:41:28 -0500

    funcionalidad de abrir quiz creada, pendiente corregir error de redireccion

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,13 @@
 //IR AL QUIZ
-const btnstarQ = document.getElementById('ComenzarQ')
+// const btnstarQ = document.getElementById('ComenzarQ')
 
-btnstarQ.addEventListener('click', Goquiz)
+// btnstarQ.addEventListener('click', Goquiz)
 
-function Goquiz() {
-    window.location.assign("/pages/quiz.html")
-}
+// function Goquiz() {
+//     window.location.assign("/pages/quiz.html")
+// }
 
+//FUNCIONALIDAD DE MOSTRAR LAS PREGUNTAS
+document.getElementById("cardBtn").addEventListener("click", () => {
+    document.getElementById("container").classList.toggle("change")
+})
