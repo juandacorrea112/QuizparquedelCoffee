ad64f5a08981d398a5d433065b01ffbb1ba5c066
juandavidad <juandavidcorreaorozco74@gmail.com>
2023-03-11 15:33:38 -0500

    lluvia al page de loser player agragada

---
+++ scripts/loserPlayer.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.contLoserP')
+
+const rain = () => {
+    let j = 0
+
+    while (j <= 80) {
+        let gout = document.createElement('i')
+        let x = innerWidth * Math.random()
+        let time = 1 * Math.random()
+
+        setTimeout(() => {
+            gout.style.animationDuration = time + 's'
+            gout.style.left = x + 'px'
+        }, 2100);
+        
+
+        container.appendChild(gout)
+
+        j++
+    }
+}
+
+rain()
