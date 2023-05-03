84e0425f4fc90bc69366d3366417269db299568c
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-03 15:43:56 -0500

    codigo innecesario eliminado

--- scripts/winner.js
+++ scripts/winner.js
@@ -1,21 +1,3 @@
-// let finalPoints = document.getElementById('number')
-
-// window.addEventListener('load', () => {
-
-//     const fetchData = async () => {
-//         const res = await fetch('/json/calificacion.json')
-//         const data = await res.json()
-//         plasmar(data)
-//         // return data
-//         console.log(data[0].calificacion);
-
-//     }
-//     fetchData()
-// })
-// function plasmar(data) {
-//     finalPoints.textContent = data[0].calificacion
-// }
-
 window.addEventListener('load', () => {
     setTimeout(() => {
         document.getElementById('lentDarkWinner').classList.toggle('lentDarkWinner')
