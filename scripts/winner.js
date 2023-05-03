933c61e03c14f8597937d9a3e9dad39272ee4f99
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-03 11:20:03 -0500

    errores solucionados y transicion arreglada

--- scripts/winner.js
+++ scripts/winner.js
@@ -1,17 +1,36 @@
-let finalPoints = document.getElementById('number')
+// let finalPoints = document.getElementById('number')
+
+// window.addEventListener('load', () => {
+
+//     const fetchData = async () => {
+//         const res = await fetch('/json/calificacion.json')
+//         const data = await res.json()
+//         plasmar(data)
+//         // return data
+//         console.log(data[0].calificacion);
+
+//     }
+//     fetchData()
+// })
+// function plasmar(data) {
+//     finalPoints.textContent = data[0].calificacion
+// }
 
 window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.getElementById('lentDarkWinner').classList.toggle('lentDarkWinner')
+        document.getElementById('lentDarkWinner').classList.toggle('lentDarkRemoveWinner')
 
-    const fetchData = async () => {
-        const res = await fetch('/json/calificacion.json')
-        const data = await res.json()
-        plasmar(data)
-        // return data
-        console.log(data[0].calificacion);
+    }, 1000);
 
-    }
-    fetchData()
+    setTimeout(() => {
+        document.getElementById('lentDarkWinner').classList.toggle('lentDarkRemoveWinner')
+        document.getElementById('lentDarkWinner').classList.toggle('lentDarkWinner')
+        document.getElementById('Felicidades').classList.toggle('Felicidades')
+        document.getElementById('ganaste').classList.toggle('ganaste')
+    }, 15000);
+
+    setTimeout(() => {
+        window.location.assign('/pages/index.html')
+    }, 20000);
 })
-function plasmar(data) {
-    finalPoints.textContent = data[0].calificacion
-}
